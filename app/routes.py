e4208110690315325be53fe19a3bf8e619575c6b
Louiz77 <luizgu009@gmail.com>
2024-10-30 15:48:10 -0300

    implement: add attempts to requests/post

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+import time
 from .pipefy_service import PipefyService
 from .zabbix_service import ZabbixService
 from .whatsapp_service import WhatsappService
@@ -7,6 +8,25 @@ zabbix_bp = Blueprint('zabbix', __name__)
 zabbix_service = ZabbixService()
 zabbix_service.load_mapping_from_file()
 
+RETRY_LIMIT = 5
+# Número máximo de tentativas
+
+RETRY_DELAY = 5
+# Tempo de espera entre as tentativas (em segundos)
+
+def attempt_with_retries(func, *args, **kwargs):
+    """Tenta executar uma função com um limite de tentativas em caso de falha."""
+    attempts = 0
+    while attempts < RETRY_LIMIT:
+        response = func(*args, **kwargs)
+        if response.get("status_code", 200) == 200:
+            # Se o status for 200, retorna a resposta
+            return response
+        attempts += 1
+        print(f"Attempt {attempts} failed. Retrying in {RETRY_DELAY} seconds...")
+        time.sleep(RETRY_DELAY)
+    return response
+    # Retorna a última resposta após atingir o limite de tentativas
 
 @zabbix_bp.route('/zabbix-webhook', methods=['POST'])
 def handle_zabbix_webhook():
@@ -44,21 +64,23 @@ def handle_zabbix_webhook():
     whatsapp_service = WhatsappService()
 
     #card no Pipefy
-    response = pipefy_service.create_card(title, description)
-    print(f"Response from Pipefy API: {response}")
-
-    if not response or 'errors' in response:
+    response = attempt_with_retries(pipefy_service.create_card, title, description)
+    if 'errors' in response:
+        message = f"Nao foi possível criar o card no Pipefy\n\nErro: {response}"
+        whatsapp_service.sendMessage(message, session_id="1")
         return jsonify({'error': 'Failed to create card in Pipefy', 'details': response}), 500
 
     try:
         card_id = response['data']['createCard']['card']['id']
         zabbix_service.save_card_mapping(data.get('trigger_id'), card_id)
     except (KeyError, TypeError) as e:
+        message = f"Nao foi possível criar o card no Pipefy\n\nErro: {str(e)}"
+        whatsapp_service.sendMessage(message, session_id="1")
         return jsonify({'error': 'Unexpected response structure from Pipefy', 'details': str(e)}), 500
 
-    session_id = f"1"
+    # Enviar mensagem no WhatsApp com tentativas de reenvio
     message = f"{description}"
-    whatsapp_service.sendMessage(message, session_id)
+    attempt_with_retries(whatsapp_service.sendMessage, message, session_id="1")
 
     return jsonify({'message': 'Card created successfully', 'card_id': card_id}), 200
 
@@ -89,17 +111,21 @@ def handle_zabbix_resolved():
     card_id = zabbix_service.get_card_id_by_trigger(data.get('trigger_id'))
 
     if not card_id:
+        session_id = f"1"
+        message = f"Nao foi possível mover o card para concluido no Pipefy\n\nErro: ID do card não foi encontrado"
+        whatsapp_service.sendMessage(message, session_id)
         return jsonify({'error': 'Card ID not found for the given trigger ID'}), 404
 
     pipefy_service = PipefyService()
 
-    response = pipefy_service.move_card_to_phase(card_id)
-
+    response = attempt_with_retries(pipefy_service.move_card_to_phase, card_id)
     if 'errors' in response:
+        message = f"Erro ao mover o card no pipefy\n\nErro: {response}"
+        whatsapp_service.sendMessage(message, session_id="1")
         return jsonify({'error': 'Failed to move card in Pipefy', 'details': response}), 500
 
-    session_id = f"1"
+    # Enviar mensagem de resolução no WhatsApp com tentativas de reenvio
     message = f"{title} - {description}"
-    whatsapp_service.sendMessageResolved(message, session_id)
+    attempt_with_retries(whatsapp_service.sendMessageResolved, message, session_id="1")
 
     return jsonify({'message': 'Card moved successfully'}), 200
