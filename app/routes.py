f82ece602c868059b140077c7699bbfbac37e32c
Louiz77 <luizgu009@gmail.com>
2024-10-31 10:52:31 -0300

    debug

--- app/routes.py
+++ app/routes.py
@@ -48,18 +48,21 @@ def handle_zabbix_webhook():
     print(f"Response from Pipefy API: {response}")
 
     if not response or 'errors' in response:
+        print(f"Response: {response}")
         return jsonify({'error': 'Failed to create card in Pipefy', 'details': response}), 500
 
     try:
         card_id = response['data']['createCard']['card']['id']
         zabbix_service.save_card_mapping(data.get('trigger_id'), card_id)
     except (KeyError, TypeError) as e:
+        print(f"Response: {response}")
         return jsonify({'error': 'Unexpected response structure from Pipefy', 'details': str(e)}), 500
 
     session_id = f"1"
     message = f"{description}"
     whatsapp_service.sendMessage(message, session_id)
 
+    print(f"Response: {response}")
     return jsonify({'message': 'Card created successfully', 'card_id': card_id}), 200
 
 
@@ -96,6 +99,7 @@ def handle_zabbix_resolved():
     response = pipefy_service.move_card_to_phase(card_id)
 
     if 'errors' in response:
+        print(f"Response: {response}")
         return jsonify({'error': 'Failed to move card in Pipefy', 'details': response}), 500
 
     session_id = f"1"
