7934393ecd9f589ddf16680245af7f4be9e57e1e
Louiz77 <luizgu009@gmail.com>
2024-10-31 09:33:41 -0300

    fix: error to msg

--- app/routes.py
+++ app/routes.py
@@ -9,24 +9,24 @@ zabbix_service = ZabbixService()
 zabbix_service.load_mapping_from_file()
 
 RETRY_LIMIT = 5
-# Número máximo de tentativas
-
-RETRY_DELAY = 5
-# Tempo de espera entre as tentativas (em segundos)
+RETRY_DELAY = 5  # segundos de espera entre tentativas
 
 def attempt_with_retries(func, *args, **kwargs):
-    """Tenta executar uma função com um limite de tentativas em caso de falha."""
+    """Executa a função com tentativas, em caso de erro."""
+    whatsapp_service = WhatsappService()
     attempts = 0
     while attempts < RETRY_LIMIT:
         response = func(*args, **kwargs)
-        if response.get("status_code", 200) == 200:
-            # Se o status for 200, retorna a resposta
+        if response.get("status_code") == 200:
+            # Retorna a resposta se o status for 200
             return response
         attempts += 1
         print(f"Attempt {attempts} failed. Retrying in {RETRY_DELAY} seconds...")
         time.sleep(RETRY_DELAY)
+        if attempts == 5:
+            message = f"Nao foi possível encontrar as informações do erro\n\nErro: {response}\n\nVerificar o Zabbix para confirmar que não há grandes problemas"
+            whatsapp_service.sendMessage(message, session_id="1")
     return response
-    # Retorna a última resposta após atingir o limite de tentativas
 
 @zabbix_bp.route('/zabbix-webhook', methods=['POST'])
 def handle_zabbix_webhook():
@@ -38,32 +38,31 @@ def handle_zabbix_webhook():
     item_name = data.get('item_name', 'Item monitorado nao disponivel')
     item_value = data.get('item_value', 'Valor atual nao disponivel')
     problem_duration = data.get('problem_duration', 'Duracao nao disponivel')
-    last_check_in = data.get('last_check_in', 'Ultimo check-in nao disponivel')	
+    last_check_in = data.get('last_check_in', 'Ultimo check-in nao disponivel')
 
     problem = data.get('problem', '')
     problem_escaped = problem.replace('"', '\\"')
-
     title = problem_escaped
 
     description = (
-	f"🚨 *ALERTA DE PROBLEMA* 🚨\n\n"
-	f"⚠️ *Status*: _{data.get('trigger_status', 'Status nao disponivel')}_\n"
-	f"🔧 *Titulo do Problema*: _{title}_\n"
-        f"🖥️ *Host*: _{data.get('host', 'Host nao disponivel')}_\n"
-        f"🌐 *IP*: _{host_ip}_\n"
-        f"🏷️ *Descricao da Maquina*: _{host_description}_\n"
-        f"📍 *Trigger ID*: _{data.get('trigger_id', 'ID nao disponivel')}_\n"
-        f"📊 *Severidade*: _{severity}_\n"
-        f"🕵️ *Item Monitorado*: _{item_name}_\n"
-        f"📈 *Valor Atual*: _{item_value}_\n"
-        f"🔁 *Duracao do Problema*: _{problem_duration}_\n"
-        f"📅 *Ultimo Check-In do Host*: _{last_check_in}_\n"
+        f"🚨 ALERTA DE PROBLEMA 🚨\n\n"
+        f"⚠️ Status: {data.get('trigger_status', 'Status nao disponivel')}\n"
+        f"🔧 Titulo do Problema: {title}\n"
+        f"🖥️ Host: {data.get('host', 'Host nao disponivel')}\n"
+        f"🌐 IP: {host_ip}\n"
+        f"🏷️ Descricao da Maquina: {host_description}\n"
+        f"📍 Trigger ID: {data.get('trigger_id', 'ID nao disponivel')}\n"
+        f"📊 Severidade: {severity}\n"
+        f"🕵️ Item Monitorado: {item_name}\n"
+        f"📈 Valor Atual: {item_value}\n"
+        f"🔁 Duracao do Problema: {problem_duration}\n"
+        f"📅 Ultimo Check-In do Host: {last_check_in}\n"
     )
 
     pipefy_service = PipefyService()
     whatsapp_service = WhatsappService()
 
-    #card no Pipefy
+    # Criação de card no Pipefy com tentativas de reenvio
     response = attempt_with_retries(pipefy_service.create_card, title, description)
     if 'errors' in response:
         message = f"Nao foi possível criar o card no Pipefy\n\nErro: {response}"
@@ -94,7 +93,6 @@ def handle_zabbix_resolved():
 
     problem = data.get('problem', '')
     problem_escaped = problem.replace('"', '\\"')
-
     title = problem_escaped
 
     description = (
@@ -102,7 +100,7 @@ def handle_zabbix_resolved():
         f"IP: {host_ip} \n"
         f"Descricao: {host_description} \n"
         f"Trigger ID: {data.get('trigger_id')} \n"
-        f"Trigger Status: *{data.get('trigger_status')}*"
+        f"Trigger Status: {data.get('trigger_status')}"
     )
 
     whatsapp_service = WhatsappService()
@@ -111,13 +109,14 @@ def handle_zabbix_resolved():
     card_id = zabbix_service.get_card_id_by_trigger(data.get('trigger_id'))
 
     if not card_id:
-        session_id = f"1"
+        session_id = "1"
         message = f"Nao foi possível mover o card para concluido no Pipefy\n\nErro: ID do card não foi encontrado"
         whatsapp_service.sendMessage(message, session_id)
         return jsonify({'error': 'Card ID not found for the given trigger ID'}), 404
 
     pipefy_service = PipefyService()
 
+    # Mover card no Pipefy com tentativas de reenvio
     response = attempt_with_retries(pipefy_service.move_card_to_phase, card_id)
     if 'errors' in response:
         message = f"Erro ao mover o card no pipefy\n\nErro: {response}"
