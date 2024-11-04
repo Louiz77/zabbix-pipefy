5da1f7b7a004afedff205256c4be3c0df9d6fc5c
Louiz77 <luizgu009@gmail.com>
2024-11-04 12:11:21 -0300

    fix: char "" problem

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,44 @@ from flask import Blueprint, request, jsonify
 from .pipefy_service import PipefyService
 from .zabbix_service import ZabbixService
 from .whatsapp_service import WhatsappService
+import json
+import re
 
 zabbix_bp = Blueprint('zabbix', __name__)
 zabbix_service = ZabbixService()
 zabbix_service.load_mapping_from_file()
 
+def clean_json_string(json_string):
+    """
+    Substitui apenas aspas duplas internas duplicadas em valores específicos do JSON por aspas simples,
+    sem adicionar caracteres de escape.
+    """
+    # Substitui aspas duplas internas nos campos 'problem' e 'item_name' por aspas simples
+    json_string = re.sub(r'("problem":\s*")([^"]*?)"([^"]*?)"([^"]*?")', r'\1\2\3 \4', json_string)
+    json_string = re.sub(r'("item_name":\s*")([^"]*?)"([^"]*?)"([^"]*?")', r'\1\2\3 \4', json_string)
+    return json_string
 
 @zabbix_bp.route('/zabbix-webhook', methods=['POST'])
 def handle_zabbix_webhook():
-    data = request.json
+    try:
+        raw_data = request.data.decode('utf-8')
+        print("JSON bruto recebido:", raw_data)
 
-    host_ip = data.get('ip', 'IP nao disponivel')
-    host_description = data.get('description', 'Descricao nao disponivel')
+        cleaned_data_str = clean_json_string(raw_data)
+        print("JSON após limpeza:", cleaned_data_str)
+
+        data = json.loads(cleaned_data_str)
+    except json.JSONDecodeError as e:
+        # Erro específico de JSON
+        print("Erro ao decodificar JSON:", e)
+        return jsonify({'error': 'Falha ao decodificar JSON', 'details': str(e)}), 400
+    except Exception as e:
+        # Outros erros genéricos
+        print("Erro ao processar dados:", e)
+        return jsonify({'error': 'Falha ao processar dados', 'details': str(e)}), 500
+
+    host_ip = data.get('host_ip', 'IP nao disponivel')
+    host_description = data.get('host_description', 'Descricao nao disponivel')
     severity = data.get('severity', 'Severidade nao disponivel')
     item_name = data.get('item_name', 'Item monitorado nao disponivel')
     item_value = data.get('item_value', 'Valor atual nao disponivel')
@@ -39,13 +65,15 @@ def handle_zabbix_webhook():
         f"🔁 *Duracao do Problema*: _{problem_duration}_\n"
         f"📅 *Ultimo Check-In do Host*: _{last_check_in}_\n"
     )
-
     pipefy_service = PipefyService()
     whatsapp_service = WhatsappService()
 
     #card no Pipefy
-    response = pipefy_service.create_card(title, description)
-    print(f"Response from Pipefy API: {response}")
+    try:
+        response = pipefy_service.create_card(title, description)
+        print(f"Response from Pipefy API: {response}")
+    except Exception as e:
+        print(e)
 
     if not response or 'errors' in response:
         print(f"Response: {response}")
@@ -60,6 +88,7 @@ def handle_zabbix_webhook():
 
     session_id = f"1"
     message = f"{description}"
+    print(message)
     whatsapp_service.sendMessage(message, session_id)
 
     print(f"Response: {response}")
